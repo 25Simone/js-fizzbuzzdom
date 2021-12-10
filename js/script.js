1f3c284755173b677429fc1e45e8b536010296a7
25Simone <dantsimone111@gmail.com>
2021-12-10 15:19:42 +0100

    milestone2 completato

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
+// Dichiaro la variabile che mi collega all html del boxes-container
+const boxesContainer = document.querySelector('.boxes-container');
+
 // Utilizzo un ciclo for per contare fino a 100
-// imposto if ed else per soddisfare le condizioni della traccia
 for (let i = 1; i <= 100; i++) {
+    // imposto if ed else per soddisfare le condizioni della traccia
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
+        boxesContainer.innerHTML += '<div class="box">FizzBuzz</div>';
     } else if (i % 3 === 0) {
-        console.log('Fizz');
+        boxesContainer.innerHTML += '<div class="box">Fizz</div>';
     } else if (i % 5 === 0) {
-        console.log('Buzz');
+        boxesContainer.innerHTML += '<div class="box">Buzz</div>';
     } else {
-        console.log(i);
+        boxesContainer.innerHTML += `<div class="box">${i}</div>`;
     }
 }
