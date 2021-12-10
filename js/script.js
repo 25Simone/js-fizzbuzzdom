e6117ba424f102c7198a6adcb10b6464cf070e10
25Simone <dantsimone111@gmail.com>
2021-12-10 15:29:55 +0100

    milestone3 completato

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ const boxesContainer = document.querySelector('.boxes-container');
 for (let i = 1; i <= 100; i++) {
     // imposto if ed else per soddisfare le condizioni della traccia
     if (i % 3 === 0 && i % 5 === 0) {
-        boxesContainer.innerHTML += '<div class="box">FizzBuzz</div>';
+        boxesContainer.innerHTML += '<div class="box fizzbuzz">FizzBuzz</div>';
     } else if (i % 3 === 0) {
-        boxesContainer.innerHTML += '<div class="box">Fizz</div>';
+        boxesContainer.innerHTML += '<div class="box fizz">Fizz</div>';
     } else if (i % 5 === 0) {
-        boxesContainer.innerHTML += '<div class="box">Buzz</div>';
+        boxesContainer.innerHTML += '<div class="box buzz">Buzz</div>';
     } else {
-        boxesContainer.innerHTML += `<div class="box">${i}</div>`;
+        boxesContainer.innerHTML += `<div class="box i">${i}</div>`;
     }
 }
